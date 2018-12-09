ecefa71aa062aa220547cfd14646f9eb2e462847
git_lm <git_lm@163.com>
2018-12-09 11:23:42 +0800

    2018-12-9

--- application/store/init.php
+++ application/store/init.php
@@ -17,7 +17,7 @@ Route::rule('wx<_?>', function (Request $request)
 // 微信菜单链接配置
 //$GLOBALS['WechatMenuLink'][] = ['link' => '@wx', 'title' => '微信商城首页'];
 //$GLOBALS['WechatMenuLink'][] = ['link' => '@wx-demo-jsapi', 'title' => 'JSAPI支付测试'];
-$GLOBALS['WechatMenuLink'][] = ['link' => url(), 'title' => '动作库首页'];
+$GLOBALS['WechatMenuLink'][] = ['link' => url('', '', true, true), 'title' => '动作库首页'];
 // @todo 模块处理机制将写在下面（包括模块初始化及升级）
 // @todo 模块权限处理，使用全局数组
 // @todo 模板菜单处理，默认放到全局数组中，然后在菜单中可以快速编辑（还要考虑下）
