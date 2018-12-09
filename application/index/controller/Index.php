23c36938d8d6b8012bb8bec3c244ce4e6b1cdec3
git_lm <git_lm@163.com>
2018-12-09 09:54:22 +0800

    2018-12-9

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,11 +8,14 @@ use think\Controller;
  * 应用入口控制器
  * @author Anyon 
  */
-class Index extends Controller {
+class Index extends Controller
+{
 
-    public function initialize() {
+    public function initialize()
+    {
         // 登录状态检查
-        if (!session('motion_member')) {
+        if (!session('motion_member'))
+        {
             $msg = ['code' => 0, 'msg' => '抱歉，您还没有登录获取访问权限！', 'url' => url('/login')];
             return request()->isAjax() ? json_encode($msg) : $this->redirect($msg['url']);
         }
@@ -21,11 +24,16 @@ class Index extends Controller {
     /**
      *  首页
      */
-    public function index() {
+    public function index()
+    {
+        
+        
         $type = request()->has('type', 'get') ? request()->get('type/s') : '';
-        if ($type) {
+        if ($type)
+        {
             $this->assign('type', $type);
-        } else {
+        } else
+        {
             $this->assign('type', 'upcomming');
         }
         return $this->fetch();
--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -4,49 +4,85 @@ namespace app\index\controller;
 
 use think\Controller;
 use app\motion\model\Member as memberModel;
+use service\WechatService;
 
 /**
  * 应用入口控制器
  * @author Anyon 
  */
-class Login extends Controller {
+class Login extends Controller
+{
 
-    public function initialize() {
+    public function initialize()
+    {
         $this->memberModel = new memberModel();
     }
 
     /**
      *  首页
      */
-    public function index() {
+    public function index()
+    {
 
         return $this->fetch();
     }
 
-    public function login() {
+    public function login()
+    {
         $username = request()->has('username', 'post') ? request()->post('username/s') : '';
         $password = request()->has('password', 'post') ? request()->post('password/s') : '';
-        if (!$username || !$password) {
+        if (!$username || !$password)
+        {
             $this->error('请完整输入');
         }
         $where[] = ['name|phone', '=', $username];
         $member = $this->memberModel->get_member($where);
         empty($member) && $this->error('登录账号不存在，请重新输入!');
-        if ($member['password'] != md5($password)) {
+        if ($member['password'] != md5($password))
+        {
             $this->error('登录密码错误，请重新输入!');
         }
-        if ($member['status'] == -1) {
+        if ($member['status'] == -1)
+        {
             $this->error('账号已经被禁用，请联系管理员!');
         }
         session('motion_member', $member);
         $this->memberModel->write('登录系统', '用户登录系统成功', $member['name'], $member['id']);
-        $this->success('登录成功，正在进入系统...', '/list.html');
+        if (!$this->is_weixin())
+        {
+            $urlString = WechatService::WeChatOauth()->getOauthRedirect(url('/index/login/wxinfo'));
+            $this->success('登录成功，正在进入系统...', $urlString);
+        } else
+        {
+            $this->success('登录成功，正在进入系统...', '/list.html');
+        }
+    }
+
+    public function wxinfo()
+    {
+        $info = WechatService::WeChatOauth()->getOauthAccessToken();
+        dump($info);
+    }
+
+    /**
+     * 判断是否微信
+     * @return boolean
+     */
+    function is_weixin()
+    {
+        if (strpos($_SERVER['HTTP_USER_AGENT'],
+                        'MicroMessenger') !== false)
+        {
+            return true;
+        }
+        return false;
     }
 
     /**
      * 退出登录
      */
-    public function out() {
+    public function out()
+    {
         $member = session('motion_member');
         session('motion_member', NULL);
         $this->memberModel->write('退出系统', '用户退出系统成功', $member['name'], $member['id']);
