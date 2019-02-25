9a12fc1c5e6b9d95f3e47f49bcb8f805caa42503
git-lm <39265396+git-lm@users.noreply.github.com>
2019-02-25 14:22:59 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,13 +6,13 @@ return [
     // 数据库类型
     'type'     => 'mysql',
     // 服务器地址
-    'hostname' => '47.99.189.127',
+    'hostname' => 'localhost',
     // 数据库名
-    'database' => 'motion',
+    'database' => 'root',
     // 用户名
     'username' => 'root',
     // 密码
-    'password' => 'sen2Cnxx4LyEXHWw',
+    'password' => 'root',
     // 端口
     'hostport' => '3306',
 ];
