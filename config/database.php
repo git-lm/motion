ba0c451d1460f453b7612ed4279fc799b1765ab8
git_wj <git_lm@163.com>
2019-02-25 14:40:59 +0800

    添加数据库信息

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    // 数据库调试模式
+    'debug'    => true,
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '47.99.189.127',
+    // 数据库名
+    'database' => 'motion',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => 'sen2Cnxx4LyEXHWw',
+    // 端口
+    'hostport' => '3306',
+];
+
