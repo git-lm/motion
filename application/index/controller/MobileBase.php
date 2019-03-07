f1fa6f17935da009aab230e2ce6278e86f7cfd0e
git_wj <git_lm@163.com>
2019-03-07 17:20:20 +0800

    修改 登录  授权

--- application/index/controller/MobileBase.php
+++ application/index/controller/MobileBase.php
@@ -18,16 +18,20 @@ class MobileBase extends Controller
     {
         $appid = empty(sysconf('wechat_appid')) ? '' : sysconf('wechat_appid');
         if (session('motion_member')) { //说明已经登录了直接返回
+            write_log('说明已经登录了直接返回');
             return;
         }
         //微信浏览器
         if (strstr($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')) {
             if (empty(session("{$appid}_openid"))) {
                 if (sysconf('wechat_appid')) {
+
                     $openid = WechatService::webOauth(request()->url(true), 0);
+                    write_log('说明微信授权获取了openid:' . $openid);
                     $this->get_member($openid);
                 }
             } else {
+                write_log('说明已经授权获取了openid:' . $openid);
                 $this->get_member(session("{$appid}_openid"));
             }
         }
@@ -48,11 +52,13 @@ class MobileBase extends Controller
             $mwhere['id'] = $memberInfo['m_id'];
             $member = $memberModel->get_member($mwhere);
             session('motion_member', $member);
+            write_log('免登录进入');
             $memberModel->write('登录系统', '用户登录系统成功', $member['name'], $member['id'], $openid);
             $this->redirect('/list');
         } else {
             if (request()->controller() != 'Login' && request()->action() != 'login')
-                $this->redirect('/login');
+                write_log('没有登陆过，先登录');
+            $this->redirect('/login');
         }
     }
 }
