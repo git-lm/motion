ba973e78fa5d19d7e206e3327ab71c3694627b1e
git_wj <git_lm@163.com>
2019-04-11 14:59:15 +0800

    添加私教管理功能

---
+++ application/pt/controller/Member.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace app\pt\controller;
+
+use controller\BasicAdmin;
+use app\pt\model\MemberModel;
+
+class Member extends BasicAdmin
+{
+    public function initialize()
+    {
+        $this->mm = new MemberModel();
+    }
+    /**
+     * 渲染会员首页
+     */
+    public function index()
+    {
+        $this->assign('title', '会员列表');
+        return $this->fetch();
+    }
+
+    /**
+     * 首页获取会员信息
+     */
+    public function get_lists()
+    {
+        $post = input('post.');
+        $lists =  $this->mm->lists($post);
+        echo $this->tableReturn($lists->all(), $lists->total());
+    }
+    /**
+     * 新增会员
+     */
+    public function add()
+    {
+        if (request()->isGet()) {
+            return $this->fetch();
+        } else {
+            $param['name'] = input('post.name/s');
+            $param['phone'] = input('post.phone/s');
+            $this->mm->add($param);
+            if ($this->mm->error) {
+                $this->error($this->mm->error);
+            } else {
+                $this->success('新增成功', '');
+            }
+        }
+    }
+    /**
+     * 编辑会员
+     */
+    public function edit()
+    {
+        if (request()->isGet()) {
+            $mid = input('get.mid/d', 0);
+            $list = $this->mm->list(array('id' => $mid));
+            $this->assign('list', $list);
+            return $this->fetch();
+        } else {
+            $param['name'] = input('post.name/s');
+            $param['phone'] = input('post.phone/s');
+            $param['id'] = input('post.mid/s');
+            $this->mm->edit($param);
+            if ($this->mm->error) {
+                $this->error($this->mm->error);
+            } else {
+                $this->success('编辑成功', '');
+            }
+        }
+    }
+    /**
+     * 删除会员
+     */
+
+    public function del()
+    {
+        $mid = input('post.mid/s');
+        $param['status'] = 1;
+        $this->mm->updateMember($param, $mid);
+        if ($this->mm->error) {
+            $this->error($this->mm->error);
+        } else {
+            $this->success('删除成功', '');
+        }
+    }
+    /**
+     * 分配会员-教练
+     */
+    public function dis()
+    {
+        if (request()->isGet()) {
+            //获取数据
+            $mid = input('get.mid/s');
+            if (!$mid) {
+                $this->error('请正确选择会员');
+            }
+            $list = $this->mm->list(array('id' => $mid));
+            $coachModel = new \app\motion\model\Coach;
+            $where['c.status'] = 1;
+            $oders['c.create_time'] = 'desc';
+            $coachs = $coachModel->get_coachs($where, $oders);
+            $this->assign('list', $list);
+            $this->assign('coachs', $coachs);
+            return $this->fetch();
+        } else {
+            $mid =  input('post.mid/d');
+            $param['coach_id'] =  input('post.coach_id/d');
+            $this->mm->updateMember($param, $mid);
+            if ($this->mm->error) {
+                $this->error($this->mm->error);
+            } else {
+                $this->success('分配成功', '');
+            }
+        }
+    }
+}
---
+++ application/pt/model/MemberModel.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace app\pt\model;
+
+use think\Model;
+use think\Db;
+
+class MemberModel extends Model
+{
+    public $error;
+    protected $autoWriteTimestamp = 'timestamp';
+    // 定义时间戳字段名
+    protected $createTime = 'create_at';
+    protected $table = 'motion_pt_member';
+
+    /**
+     * 获取单个会员
+     */
+    public function list($param)
+    {
+        $where['status'] = ['=', 0];
+        if (!empty($param['phone'])) $where['phone'] = ['=', $param['phone']];
+        if (!empty($param['id'])) $where['id'] = ['=', $param['id']];
+        $list =  self::where($where)->find();
+
+        return $list;
+    }
+
+
+
+    /**
+     * 分页获取所有数据
+     */
+    public function lists($param)
+    {
+        $where['status'] = ['=', 0];
+        $limit = !empty($param['limit']) ? $param['limit'] : 10;
+        $lists =  self::where($where)->paginate($limit);
+        return $lists;
+    }
+
+    /**
+     * 新增会员
+     */
+    public function add($param)
+    {
+        $validate = $this->validateMember($param);
+        if ($validate) {
+            $this->error = $validate;
+            return;
+        }
+        $list = $this->list(array('phone' => $param['phone']));
+        if (!empty($list)) {
+            $this->error = '该手机号码已存在';
+            return;
+        }
+        $this->name = $param['name'];
+        $this->phone = $param['phone'];
+        $code = $this->save();
+        if ($code) {
+            return;
+        } else {
+            $this->error = '新增失败';
+        }
+    }
+
+    /**
+     * 编辑会员
+     */
+    public function edit($param)
+    {
+        $validate = $this->validateMember($param);
+        if ($validate) {
+            $this->error = $validate;
+            return;
+        }
+        $list = self::where(array('phone' => $param['phone']))->where('id', '<>', $param['id'])->find();
+        if (!empty($list)) {
+            $this->error = '该手机号码已存在';
+            return;
+        }
+        $this->updateMember($param, $param['id']);
+    }
+    /**
+     * 更新会员信息
+     */
+    public function updateMember($param, $member_id)
+    {
+        $code =  $this->where(array('id' => $member_id))->update($param);
+        if ($code) {
+            return;
+        } else {
+            $this->error = '操作失败';
+        }
+    }
+
+
+    /**
+     * 验证类型数据有效性
+     * @param type $data 需要验证的数据
+     */
+    public function validateMember($data)
+    {
+        $rule = [
+            'name' => 'require|max:5|min:2|chsAlpha',
+            'phone' => 'require|mobile',
+        ];
+        $message = [
+            'name.require' => '会员名称必填',
+            'name.min' => '会员名称最少两个字',
+            'name.max' => '会员名称最多五个字',
+            'name.chsAlpha' => '会员名称只能汉子和字母',
+            'phone.require' => '手机号码必填',
+            'phone.mobile' => '手机号码格式不正确',
+        ];
+        $validate = new \think\Validate();
+        $validate->rule($rule)->message($message)->check($data);
+        return $validate->getError();
+    }
+}
